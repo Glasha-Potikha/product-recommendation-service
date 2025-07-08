ca38dc5b5a093d5c4d8853823dea1c64ef82218a
Nikolay <nikshowsix@gmail.com>
2025-07-08 15:49:08 +0300

    Initial

---
+++ src/test/java/starBank/example/recomendationService/RecomendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package starBank.example.recomendationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
